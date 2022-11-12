551f076273f692a8687ceb44304eea6308e489c7
SanyaMittal17 <97736276+SanyaMittal17@users.noreply.github.com>
2022-11-12 08:24:54 +0530

    Update mux.vhd

--- mux.vhd
+++ mux.vhd
@@ -8,6 +8,7 @@ port(
     c : in std_logic_vector(3 downto 0);
     d : in std_logic_vector(3 downto 0);
     s0,s1: in STD_LOGIC;
+    dp: out std_logic;
     o: out std_logic_vector(3 downto 0));
 end mux_gate;
 
@@ -20,6 +21,7 @@ end component;
 
 begin
     ---x: clock_modified port map(clk=> clk, ts0=>s0, ts1=>s1);
+    dp <= (s1 and not(s0)) or (not(s0)) ;
     o(0)<=(not(s0) and not(s1) and a(0)) or (s0 and not(s1) and b(0)) or (not(s0) and s1 and c(0)) or (s0 and s1 and d(0));
     o(1)<=(not(s0) and not(s1) and a(1)) or (s0 and not(s1) and b(1)) or (not(s0) and s1 and c(1)) or (s0 and s1 and d(1));
     o(2)<=(not(s0) and not(s1) and a(2)) or (s0 and not(s1) and b(2)) or (not(s0) and s1 and c(2)) or (s0 and s1 and d(2));
